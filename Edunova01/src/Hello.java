8b0255714f457527a005dbe05efafc2acb357b1e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-02 15:14:31 +0100

    Vjezba

--- Edunova01/src/Hello.java
+++ Edunova01/src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Edunova");
 		System.out.println("Osijek");
+		System.out.println("Edunova");
+        System.out.println("Vjezba");
 		
 	}
 

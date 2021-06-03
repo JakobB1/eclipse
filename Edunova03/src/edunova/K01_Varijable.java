29359d195c9d76cf685668cb219f5de56e22e482
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-03 22:04:07 +0100

    vjezba

---
+++ Edunova03/src/edunova/K01_Varijable.java
@@ -0,0 +1,29 @@
+package edunova;
+
+public class K01_Varijable {
+
+	public static void main(String[] args) {
+		// deklarirali varijablu tipa cijeli broj(int) i dodjelili vrijednost 1
+		int i=1;
+		
+		// deklaracija varijable
+		int t;
+		
+		// dodjeljivanje vrijednosti 
+		t=7;
+		
+		// korištenje varijable 
+		System.out.println(t);
+		
+		// čitati primitivni tipovi podataka
+		//
+		
+		int k,l,m;
+		
+		int v=2,p=0;
+		
+		
+	
+	}
+
+}
---
+++ Edunova03/src/edunova/K02_PrimitivniTipovi.java
@@ -0,0 +1,25 @@
+package edunova;
+
+public class K02_PrimitivniTipovi {
+	
+	public static void main(String[] args) {
+		
+		boolean aktivan=true;
+		
+		byte brojdo127 = 6;
+		
+		short veciBroj=278;
+		
+		int broj = 27363;
+		
+		long velikiBroj = 3275428354L;
+		
+		float decimalniManji=3.14f;
+		
+		double velikiDecimalni=3e10;
+		
+		char znak='E';
+		
+	}
+
+}

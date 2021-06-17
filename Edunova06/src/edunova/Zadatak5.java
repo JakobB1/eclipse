b174cfb4a7a823b25dc5ed5eb41d29811ab5fa48
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-17 15:05:47 +0100

    zadatak5

--- Edunova06/src/edunova/Zadatak5.java
+++ Edunova06/src/edunova/Zadatak5.java
@@ -4,9 +4,37 @@ import javax.swing.JOptionPane;
 
 public class Zadatak5 {
 
-	// Program od korisnika unosi dva parna broja
-	// Program ispisuje njohov zbroj
+	public static void main(String[] args) {
 
-	
+		// Program od korisnika unosi dva parna broja
+		// Program ispisuje njohov zbroj
 
+		int a, b;
+
+		for (;;) {
+			a = Integer.parseInt(JOptionPane.showInputDialog("Unesi jedan parni broj"));
+
+			if (a % 2 == 0) {
+				break;
+			}
+
+			JOptionPane.showMessageDialog(null, "Nisi unio parni broj");
+
+		}
+
+		for (;;) {
+			b = Integer.parseInt(JOptionPane.showInputDialog("Unesi jedan parni broj"));
+
+			if (b % 2 == 0) {
+				break;
+			}
+
+			JOptionPane.showMessageDialog(null, "Nisi unio parni broj");
+		}
+
+		int sum = a + b;
+
+		System.out.println(sum);
+
+	}
 }

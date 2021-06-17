6d3a16746fe8ca3c023f447b4963152a124e8427
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-17 14:04:17 +0100

    zadatak2

--- Edunova06/src/edunova/Zadatak2.java
+++ Edunova06/src/edunova/Zadatak2.java
@@ -3,7 +3,8 @@ package edunova;
 import javax.swing.JOptionPane;
 
 public class Zadatak2 {
-	public static void main(String[] args) {
+	
+	    public static void main(String[] args) {
 
 		// program od korisnika unosi 2 decimalna broja
 		// ispisuje veæi

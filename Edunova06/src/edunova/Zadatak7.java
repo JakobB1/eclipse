95ec2e19065e05717c42581860b7d415fc9e4ee8
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-19 14:45:10 +0100

    zadatak7

--- Edunova06/src/edunova/Zadatak7.java
+++ Edunova06/src/edunova/Zadatak7.java
@@ -6,6 +6,22 @@ public class Zadatak7 {
 	// i s broj 1.
 	// Za uneseni broj x = 27353 ispišite da li je prim broj
 
-	
+	public static void main(String[] args) {
+
+		int x = 273532542;
+		boolean primBroj = true;
+		for (int i = 2; i < x; i++) {
+
+			if (x % i == 0) {
+				primBroj = false;
+				break;
+			}
+		}
+		if (primBroj) {
+			System.out.println("Broj je prim");
+		} else {
+			System.out.println("Broj nije prim");
+		}
+	}
 
 }

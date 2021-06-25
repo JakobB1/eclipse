ee0babb6408a050e3e801bf68b263c703e4bca07
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-25 12:00:51 +0100

    updateZadaci

--- Edunova06/src/edunova/Zadatak2.java
+++ Edunova06/src/edunova/Zadatak2.java
@@ -7,17 +7,6 @@ public class Zadatak2 {
 
 		// program od korisnika unosi 2 decimalna broja
 		// ispisuje veæi
-
-		float a, b;
-
-		a = Float.parseFloat(JOptionPane.showInputDialog("Unesite prvi dec broj"));
-		b = Float.parseFloat(JOptionPane.showInputDialog("Unesite drugi dec broj"));
-
-		if (a > b) {
-			System.out.println(a);
-		} else {
-			System.out.println(b);
-		}
-	}
-
+		
+}
 }

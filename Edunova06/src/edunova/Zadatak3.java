0f37b3728489a2fee7ae9e0cc4bf229a9ed8c797
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-25 12:17:17 +0100

    zadatak03

--- Edunova06/src/edunova/Zadatak3.java
+++ Edunova06/src/edunova/Zadatak3.java
@@ -2,10 +2,17 @@ package edunova;
 
 public class Zadatak3 {
 	
+	public static void main(String[] args) {
+	
 	// Program ispisuje sve brojeve od 100 do 21
 	// jedno do drugoga odvojeno zarezom
-	
-	
+
+		int i = 100;
+		int j = 21;
+
+		for (int a = i; a >= j; a--) {
+			System.out.println(a + ";");
+		}
+
+	}
 }
-
-

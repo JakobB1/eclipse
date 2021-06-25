e5375927ce1ecc75e8d6334a875b3156c1a366e2
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-25 12:18:56 +0100

    zadatak04

--- Edunova06/src/edunova/Zadatak4.java
+++ Edunova06/src/edunova/Zadatak4.java
@@ -9,5 +9,13 @@ public class Zadatak4 {
 		// Program uèitava cijele brojeve od korisnika
 		// sve dok se ne uèita broj 0. Tada se program završava
 
-	}		
+		int x;
+		for (;;) {
+			x = Integer.parseInt(JOptionPane.showInputDialog("daj mi jedan broj,ako uneses nula" + ",program zavrsava"));
+
+			if (x == 0) {
+				break;
+			}
+		}
+	}
 }

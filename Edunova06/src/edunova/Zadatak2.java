f96476074089433855860f113cf6e93149084e12
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-06-25 12:16:00 +0100

    zadatak2

--- Edunova06/src/edunova/Zadatak2.java
+++ Edunova06/src/edunova/Zadatak2.java
@@ -3,10 +3,21 @@ package edunova;
 import javax.swing.JOptionPane;
 
 public class Zadatak2 {
+	
 	public static void main(String[] args) {
 
 		// program od korisnika unosi 2 decimalna broja
 		// ispisuje veæi
 		
-}
+		float a,b;
+		
+		a = Float.parseFloat(JOptionPane.showInputDialog("Unesi prvi dec broj"));
+		b = Float.parseFloat(JOptionPane.showInputDialog("Unesi drugi dec broj"));
+		
+		if (a>b) {
+			System.out.println(a);
+		} else { 
+			System.out.println(b);
+		}
+	}
 }

11b82563ff076538a7ec2b24efb4ce3230f0e8a0
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-07 12:58:09 +0100

    primjer1

--- Edunova10/src/edunova/oop/primjer1/Start.java
+++ Edunova10/src/edunova/oop/primjer1/Start.java
@@ -3,9 +3,26 @@ package edunova.oop.primjer1;
 public class Start {
 	
 	// Klasa Dokument ima svojstva: ID, iznos, datum
-	// Klasa Raèun nasljeğuje Dokument i dodaje svojstvo: Kupac
+	// Klasa RaÄun nasljeÄ‘uje Dokument i dodaje svojstvo: Kupac
 	// Kupac ima ID, naziv i adresu
-	// Klasa Primka nasljeğuje Dokument i dodaje svojstvo: otpremnica
+	// Klasa Primka nasljeÄ‘uje Dokument i dodaje svojstvo: otpremnica
+	
+	// Kreirati po jednu instancu klasa RaÄun i Primka
+	
+	public Start() {
+		
+		
+		Racun r1 = new Racun (11, 421.45, "23.04.2021", new Kupac(11, "Ivo Ivic","Kapucinska 4"));
+		
+		Primka primka = new Primka(14 ,24.67, "08.07.2021", "otpremnica 1");
+		
+		System.out.println(primka);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		new Start();
+	}
 
-	// Kreirati po jednu instancu klasa Raèun i Primka
 }
